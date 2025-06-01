e7c6fa5f8b897a28774006b0615aec568652a7a6
rithesh-n <ritheshan18@gmail.com>
2025-06-01 23:27:11 +0530

    feat: implement protected route handling

---
+++ Frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+// routes/PrivateRoute.jsx
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const PrivateRoute = () => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return <div>Loading...</div>;
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
